5ba5e53f51392db5c2c2494e5e701a3e6313b12f
this4you <me_vlad@ukr.net>
2021-11-01 22:32:45 +0200

    Fix folders

--- CodeSS_Server/Program.cs
+++ CodeSS_Server/Program.cs
@@ -20,8 +20,8 @@ namespace CodeSS_Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
-                    .UseUrls("http://localhost:4000");
+                    webBuilder.UseStartup<Startup>();
+                    //.UseUrls("http://localhost:4000");
                 });
     }
 }
